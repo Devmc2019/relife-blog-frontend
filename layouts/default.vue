35cbf4cf375e9e29df34f31b94e0437dc15c6f92
Devmc <dev.mcrf@qq.com>
2021-05-16 22:58:34 +0800

    init nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+// 基本样式
+@import "assets/css/main";
+
+// Tailwind
+@import "assets/css/tailwind";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: Devmc
+ * Date: 2021/5/16
+ * Time: 22:23
+ */
+-->
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Context} from "@nuxt/types";
+
+export default Vue.extend({
+  components: {},
+  asyncData({app, store, route, params, query, env, isDev, isHMR, redirect, error,}: Context) {
+    return {}
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  head(this: any) {
+    return {
+      title: '',
+      meta: [
+        {hid: '', name: '', content: ''},
+      ]
+    };
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {},
+  watch: {}
+});
+</script>
+
+<style lang="scss">
+
+</style>
